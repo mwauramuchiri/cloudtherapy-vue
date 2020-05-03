2e67be477bb19fc5e0bdd01a7813358b01eae1b7
MwauraMuchiri <gramwauu@gmail.com>
2020-05-03 18:40:12 +0300

    auth page UI

--- src/views/Auth.vue
+++ src/views/Auth.vue
@@ -1,24 +1,51 @@
 <template>
   <v-app>
-    <v-container align="center" fluid class="fill-height">
-      <v-row class="text-center" vertical align="center" justify="center">
-        <v-col>
+    <v-container fluid>
+      <v-row class="text-center mt-8">
+        <v-col cols="12">
           <!-- LOGO -->
+          CLOUD THERAPY LOGO
         </v-col>
 
-        <v-col style="margin-top: 80px;">
+        <v-col cols="12" style="margin-top: 64px;">
           <v-row vertical align="center" justify="center">
-            <v-col>
+            <v-col cols="12">
               <h3 class="font-weight-bold">Hi There</h3>
               <h5 class="font-weight-light">Let's get started</h5>
             </v-col>
 
             <transition name="fade" mode="out-in" appear>
               <!-- BUTTONS -->
-              <v-col class="mt-5" v-if="!loading">
-                <v-btn class="mt-5" @click="btnClick('')"></v-btn>
-                <v-btn class="mt-5" @click="btnClick('')"></v-btn>
-                <v-btn class="mt-5" @click="btnClick('')"></v-btn>
+              <v-col cols="12" class="mt-5" v-if="!loadingPage">
+                <v-btn
+                  :width="228"
+                  x-large
+                  class="mt-8 ct-btn-facebook ct-shadow"
+                  @click="btnClick('')"
+                >
+                  <v-icon left>mdi-facebook</v-icon>
+                  Facebook
+                </v-btn>
+                <div></div>
+                <v-btn
+                  :width="228"
+                  x-large
+                  class="mt-8 ct-btn-twitter ct-shadow"
+                  @click="btnClick('')"
+                >
+                  <v-icon left>mdi-twitter</v-icon>
+                  Twittter
+                </v-btn>
+                <div></div>
+                <v-btn
+                  :width="228"
+                  x-large
+                  class="mt-8 ct-btn-google ct-shadow"
+                  @click="btnClick('')"
+                >
+                  <v-icon left>mdi-google</v-icon>
+                  Google
+                </v-btn>
               </v-col>
             </transition>
           </v-row>
@@ -28,8 +55,6 @@
   </v-app>
 </template>
 
-<style lang="scss"></style>
-
 <script>
 export default {
   name: "Auth",
@@ -37,6 +62,27 @@ export default {
     btnClick() {
       //
     }
+  },
+  created() {
+    this.loadingPage = false;
   }
 };
 </script>
+
+<style lang="scss">
+.ct-btn-facebook {
+  background-color: #28589c !important;
+}
+.ct-btn-twitter {
+  background-color: #1da1f2 !important;
+}
+.ct-btn-google {
+  background-color: #db4a39 !important;
+}
+
+.ct-btn-facebook,
+.ct-btn-twitter,
+.ct-btn-google {
+  color: #fff !important;
+}
+</style>
