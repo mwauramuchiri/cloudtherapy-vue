f6d2deddffd72efa0d8d748c94ab145683fc1ed4
MwauraMuchiri <gramwauu@gmail.com>
2020-05-13 10:43:03 +0300

    [Feature] Added User Mixin
    Co-authored-by: Allan N Jeremy <AllanJeremy@users.noreply.github.com>

---
+++ src/mixins/UserMixin.js
@@ -0,0 +1,29 @@
+export default {
+  name: "UserMixin",
+  methods: {
+    waitForUser() {
+      let currUser = this.$store.state.user.user;
+
+      // The user already exists ~ call the appropriate function
+      if (Object.keys(currUser).length !== 0) {
+        return Promise.resolve(currUser);
+      }
+
+      return new Promise((resolve, reject) => {
+        this.$store.watch(
+          state => state.user.user,
+          () => {
+            let user = this.$store.state.user.user;
+
+            if (Object.keys(user).length === 0) {
+              reject("User not found");
+            }
+
+            //* Getting here means user was found
+            resolve(user);
+          }
+        );
+      });
+    }
+  }
+};
