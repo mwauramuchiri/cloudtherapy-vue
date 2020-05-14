a0f604b912a4490d840a44e680e7fea81f64583d
AllanJeremy <aj.dev254@gmail.com>
2020-05-15 00:51:50 +0300

    [Enhancement] Added illustration to homepage

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -7,13 +7,14 @@
       id="home"
     >
       <v-row :justify="'center'" :align="'center'">
-        <v-col cols="10" offset="1" class="text-center">
+        <v-col cols="12" class="text-center mx-auto d-flex flex-column justify-content-center">
+          <img src="/img/illustrations/conversation.svg" alt="Cloud therapy home background" id="bgHome"/>
           <h1>It's okay not to be okay</h1>
           <h4>
             Share your issues with peers going through the same thing
             anonymously
           </h4>
-          <v-btn to="/auth" class="ct-shadow mt-5" color="primary" x-large>
+          <v-btn to="/auth" class="ct-shadow mt-5 btn-cta" color="primary" x-large>
             Get Started
           </v-btn>
         </v-col>
@@ -27,3 +28,19 @@ export default {
   name: "Home"
 };
 </script>
+
+<style scoped lang="scss">
+  #bgHome {
+    // min-width: 20rem;
+    // max
+    height: auto;
+    min-width:18rem;
+    max-width: 45rem;
+  }
+
+  .btn-cta{
+    width: 100%;
+    margin: auto;
+    max-width:32rem;
+  }
+</style>
