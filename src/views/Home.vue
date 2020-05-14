98e5b205e4dcf42a2b2418a98c117534c416261c
AllanJeremy <aj.dev254@gmail.com>
2020-05-15 01:04:30 +0300

    [UI] Centered home content

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,12 +2,12 @@
   <v-app>
     <v-container
       align="center"
-      class="home bg-image fill-height"
+      class="home bg-image fill-height  d-flex flex-column justify-content-center"
       fluid
       id="home"
     >
       <v-row :justify="'center'" :align="'center'">
-        <v-col cols="12" class="text-center mx-auto d-flex flex-column justify-content-center">
+        <v-col cols="12" class="text-center">
           <img src="/img/illustrations/conversation.svg" alt="Cloud therapy home background" id="bgHome"/>
           <h1>It's okay not to be okay</h1>
           <h4>
@@ -30,12 +30,15 @@ export default {
 </script>
 
 <style scoped lang="scss">
+  h1{
+    line-height: 120%;
+    margin-bottom: 1rem;
+  }
   #bgHome {
-    // min-width: 20rem;
-    // max
-    height: auto;
+    width: 120%;
     min-width:18rem;
     max-width: 45rem;
+    max-height: 50vh;
   }
 
   .btn-cta{
