ae2387d1cffbb467196dec9d4a73d74caf5af9e8
AllanJeremy <aj.dev254@gmail.com>
2020-08-27 09:02:26 +0300

    [Feature] Added getRandomArrayValues function to random util - This function gets a certain number of random array values from an array given an array of values

--- src/utils/random.js
+++ src/utils/random.js
@@ -1,4 +1,7 @@
-/** Gets a random positive integer within the range of `min`(include) to `max`(exclude) 
+//! Remove lodash as a dependency in future
+import * as _ from "lodash";
+
+/** Gets a random positive integer within the range of `min`(include) to `max`(exclude)
  * `max` is excluded, meaning the actual highest value random number is `max-1`
  * @param {Number} min The minimum random number
  * @param {Number} max The maximum random number. It is excluded and the actual max is `max-1`
@@ -8,7 +11,26 @@ const getRandomNumInRange = (min, max) => {
   min = Math.ceil(min);
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
-}
+};
+
+/** Get a random list of array values (Primitives) in an array
+ * @param {Array} array The array to select random values fromm
+ * @param {Number}numItemsToReturn The number of array items we want to return
+ * @notes This function relies on Lodash's shuffle function ~ find a way to get rid of this dependency
+ */
+const getRandomArrayValues = (array, numItemsToReturn) => {
+  if (numItemsToReturn > array.length || numItemsToReturn <= 0) {
+    throw new Error(
+      `Cannot select more values (${numItemsToReturn}) than the length of the array (${array.length}) or less than 1 random`
+    );
+  }
+
+  // Getting here means we can
+  const randomArray = _.shuffle(array).filter(
+    (val, index) => index < numItemsToReturn
+  );
+  return randomArray;
+};
 
 //* EXPORTS
-export  { getRandomNumInRange };
+export { getRandomNumInRange, getRandomArrayValues };
