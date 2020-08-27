d4998186745ea27ab4c0147adea9e4e05708b921
AllanJeremy <aj.dev254@gmail.com>
2020-08-27 09:05:21 +0300

    [Enhancement] Added conversation starters data file - Decoupling this from the logic so we can easily add/change them without having to affect the code

---
+++ src/data/conversation_starters.js
@@ -0,0 +1,24 @@
+const conversationStarterList = [
+  {
+    text: "Hey, when was the last time you cried?",
+    category: "depression",
+  },
+  {
+    text: "Do you ever sit down and wonder what happened to waldo?",
+    category: "anxiety",
+  },
+  {
+    text: "Totally random conversation starter",
+    category: "depression",
+  },
+  {
+    text: "Another unrelated one",
+    category: "depression",
+  },
+  {
+    text: "This is a test conversation starter",
+    category: "depression",
+  },
+];
+
+export default conversationStarterList;
