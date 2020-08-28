077ce5579f7c1c251af564991714881c5d861ec9
AllanJeremy <aj.dev254@gmail.com>
2020-08-28 08:57:20 +0300

    [Enhancement] Updated conversation starters

--- src/data/conversation_starters.js
+++ src/data/conversation_starters.js
@@ -1,22 +1,44 @@
 const conversationStarterList = [
   {
-    text: "Hey, when was the last time you cried?",
+    text: "When is the last time you truly laughed?",
     category: "depression",
   },
   {
-    text: "Do you ever sit down and wonder what happened to waldo?",
+    text: "When is the last time you cried?",
     category: "anxiety",
   },
   {
-    text: "Totally random conversation starter",
+    text: "Who is someone you lean on the most?",
     category: "depression",
   },
   {
-    text: "Another unrelated one",
+    text:
+      "What is something you wish people would pay attention to more about you?",
     category: "depression",
   },
   {
-    text: "This is a test conversation starter",
+    text: "If you had one wish, what would it be?",
+    category: "depression",
+  },
+  {
+    text: "Are you a talker or a hoarder?",
+    category: "depression",
+  },
+  {
+    text: "What to do you to change your mood?",
+    category: "depression",
+  },
+  {
+    text: "What do you think about the most when you're alone?",
+    category: "depression",
+  },
+  {
+    text:
+      "What is something you wish you could change -if anything-about yourself?",
+    category: "depression",
+  },
+  {
+    text: "Have you ever been to therapy? or What do you think about therapy",
     category: "depression",
   },
 ];
