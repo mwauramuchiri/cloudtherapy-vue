05731d14a8cb2cb20e373b0bb606ec869ec6c4f9
mwauramuchiri <gramwauu@gmail.com>
2021-04-04 20:12:22 +0300

    site footer date update

--- src/components/templates/SiteFooter.vue
+++ src/components/templates/SiteFooter.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
-<v-container class="text-center">
-      <small class="secondary--text">&copy; 2020 A Dark Binary &amp; E-Mentoring Africa initiative</small>
+    <v-container class="text-center">
+      <small class="secondary--text">&copy; 2020 - 2021 A Dark Binary &amp; E-Mentoring Africa initiative</small>
     </v-container>
   </div>
 </template>
