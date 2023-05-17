60d36f731d7923a045004a8db48f3dec1886f633
Allan N Jeremy <AllanJeremy@users.noreply.github.com>
2023-05-17 21:22:10 +0300

    [Chore] Removed unnecessary / at the end of cloudtherapy-vue

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,4 +1,4 @@
 export const SITE_NAME = "Cloud Therapy";
 
 export const BASE_API_URL = "https://cloudtherapy.herokuapp.com";
-export const CHAT_API_URL = "https://cloudtherapy.herokuapp.com/";
+export const CHAT_API_URL = "https://cloudtherapy.herokuapp.com";
