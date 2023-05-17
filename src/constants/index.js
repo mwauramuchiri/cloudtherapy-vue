a6d176c6362cd2d903fff0a02dac03d73972412a
Allan N Jeremy <AllanJeremy@users.noreply.github.com>
2023-05-17 21:21:03 +0300

    Updated API url

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,4 +1,4 @@
 export const SITE_NAME = "Cloud Therapy";
 
-export const BASE_API_URL = "https://cloudtherapy-backend.herokuapp.com";
-export const CHAT_API_URL = "https://cloudtherapy-backend.herokuapp.com";
+export const BASE_API_URL = "https://cloudtherapy.herokuapp.com";
+export const CHAT_API_URL = "https://cloudtherapy.herokuapp.com/";
