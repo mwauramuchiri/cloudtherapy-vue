0c2d6c885ee48e90425abaf04b6e81565840929b
mwauramuchiri <gramwauu@gmail.com>
2024-08-17 19:04:05 +0300

    use .env variables

--- src/constants/index.js
+++ src/constants/index.js
@@ -2,5 +2,5 @@ export const SITE_NAME = "Cloud Therapy";
 
 // export const BASE_API_URL = "https://cloudtherapy.herokuapp.com";
 // export const CHAT_API_URL = "https://cloudtherapy.herokuapp.com";
-export const BASE_API_URL = "http://localhost:5005";
-export const CHAT_API_URL = "http://localhost:5005";
+export const BASE_API_URL = process.env.VUE_APP_BASE_API_URL;
+export const CHAT_API_URL = process.env.VUE_APP_BASE_API_URL;
